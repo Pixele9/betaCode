c4bc789896b7e5b937575a5c7e522b916215bab3
Uriel <urimeba511@gmail.com>
2019-12-06 10:50:13 -0600

    *added * Flujo de registro

--- Apps/Archivos/urls.py
+++ Apps/Archivos/urls.py
@@ -5,5 +5,8 @@ from Apps.Archivos import views as views_archivos
 urlpatterns = [
     path('', views_archivos.loginn, name="loginn"),
     path('register', views_archivos.register, name="register"),
+    path('forgot', views_archivos.forgot, name="forgot"),
+
+    # path('index', views_archivos.index, name="index"),
 
 ]
--- Apps/Archivos/views.py
+++ Apps/Archivos/views.py
@@ -4,21 +4,16 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
-# from rest_framework.decorators import action
+from rest_framework.decorators import action
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
-    HTTP_404_NOT_FOUND,
-    HTTP_200_OK
-)
-from django.db.models import Q
-
+from rest_framework.status import (HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK)
 from Apps.Archivos.models import Archivo, tipoArchivo, UsuarioArchivo
 from Apps.Archivos.serializers import ArchivoSerializer, TipoArchivoSerializer, UsuarioArchivoSerializer
+from django.contrib.auth.decorators import login_required
 
 class ArchivoViewSet(viewsets.ModelViewSet):
     queryset = Archivo.objects.all()
@@ -51,8 +46,9 @@ def inicio(request):
 
     login(request, user)
     token, created = Token.objects.get_or_create(user=user)
-    print(created, token)
-    return Response({ "id":str(user.id), "token":token.key}, status=HTTP_200_OK)
+
+    request.session['token'] = str(token)
+    return Response({"Exito":"Correcto"}, status=HTTP_200_OK)
 
 
 @csrf_exempt
@@ -66,13 +62,18 @@ def registro(request):
     password = request.data.get("password")
 
     if username=="" or first_name=="" or last_name=="" or email=="" or password=="":
+        print("a")
         return Response({"Error":"Favor de completar todos los campos"}, status=HTTP_400_BAD_REQUEST)
     else:
         if User.objects.filter(email=email).exists():
+            print("b")
             return Response({"Error": "El correo ya ha sido usado"}, status=HTTP_400_BAD_REQUEST)
         elif len(password)<6:
+            print("c")
             return Response({"Error": "La contraseña debe tener minimo 6 caracteres"}, status=HTTP_400_BAD_REQUEST)
         elif len(username)<4:
+            print(len(username))
+            print("d")
             return Response({"Error": "El usuario debe tener minimo 4 caracteres "}, status=HTTP_400_BAD_REQUEST)
         else:
             user, created = User.objects.get_or_create(username=username)
@@ -82,6 +83,7 @@ def registro(request):
                 user.save()
                 return Response({"Registrado": "Usuario registrado exitosamente"}, status=HTTP_200_OK)
             else:
+                print("e")
                 return Response({"Error": "El usuario ya existe"}, status=HTTP_400_BAD_REQUEST)
 
 
@@ -90,3 +92,9 @@ def loginn(request):
 
 def register(request):
     return render(request, "signup.html")
+
+def forgot(request):
+    return render(request, "pass.html")
+
+# def index(request):
+#     return render(request, "index.html")
