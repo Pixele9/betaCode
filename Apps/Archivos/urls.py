d110ee9943aed00ad97a12b7e18289c7beac6fae
Pablo Samano <psamano25@alumnos.uaq.mx>
2019-12-11 11:07:09 -0600

    Deleted all v2

--- Apps/Archivos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from Apps.Archivos import views as views_archivos
-
-urlpatterns = [
-    path('', views_archivos.loginn, name="loginn"),
-    path('register', views_archivos.register, name="register"),
-
-]
