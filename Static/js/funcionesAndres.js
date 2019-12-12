704dd0c6698b2bd531670cae58e29280bb380d5c
Andres <pixele9@gmail.com>
2019-12-11 22:54:06 -0600

    Compilado en consola del website

--- Static/js/funcionesAndres.js
+++ Static/js/funcionesAndres.js
@@ -1,3 +1,20 @@
+// Function para obtener el token
+function getCookie(name) {
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {  
+			var cookie = cookies[i].trim();
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
+
 function setupEditor() {
 	// let editor = document.getElementById("editor")
 	window.editor = ace.edit("editor");
@@ -33,3 +50,46 @@ hello("Andres")`,1); //1 = moves cursor to end
 // }
 setupEditor()
 // update()
+
+function compile() {
+	let editor = document.getElementById("editor");
+	code = editor.innerText;
+	let numeroLineas = document.getElementsByClassName("ace_gutter-cell");
+	numeroLineas = numeroLineas.length;
+	numeroLineas=numeroLineas*2;
+	code = code.substring(numeroLineas, code.length)
+	console.log(code);
+
+	let url = "http://148.220.209.116:8000/compiler";
+	let token = getCookie('csrftoken');
+
+	settings = {
+		"lang": "Python3",
+		"input": "",
+		"code": code,
+		"save": false
+	}
+
+	$.ajax({ 
+		type: 'POST',
+		url: url,
+		data: {
+			settings:JSON.stringify(settings), 
+			csrfmiddlewaretoken: token,
+			code2Compile: code
+		},
+		success: function(data){
+			console.log("%c"+data, "font-size:30px;color:green;");
+			// setTimeout(function(){ 
+			// 	let console_out = document.getElementById("console-output")
+			// 	console_out.innerText += data
+			// 	alert("YA SE TUVO QUE ACTUALIZAR")
+			//  }, 1000);
+			let console_out = document.getElementById("console-output")
+			console_out.innerText = data
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) { 
+		} 
+	});
+	
+}
